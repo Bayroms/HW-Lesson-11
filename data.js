2b5cdd7d885af3c61a99c61f13ecc7e187039e5e
roman <roman3668@gmail.com>
2023-01-29 21:35:20 +0300

    HW Lesson 11

---
+++ data.js
@@ -0,0 +1,17 @@
+const dataInfo = `[{
+    "id": "MANGO PEOPLE T-SHIRT",
+    "image": "image/content_img1.jpg",
+    "price": " $300",
+    "color": "Color: Red",
+    "size": "Size: Xl",
+    "quantity": "2"
+},
+{
+    "id": "ELLERY X M'O CAPSULE",
+    "image": "image/content_img2.jpg",
+    "price": " $52",
+    "color": "Color: black",
+    "size": "Size: Xl",
+    "quantity": "1"
+}
+]`
